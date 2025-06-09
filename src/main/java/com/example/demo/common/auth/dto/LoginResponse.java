5795d85824163a7f239d7bded585501da3e7f6d2
2jiyong <jdragon2000@naver.com>
2025-06-09 14:03:39 +0900

    feat: 로그인 dto 추가

---
+++ src/main/java/com/example/demo/common/auth/dto/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo.common.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+  private String accessToken;
+}
