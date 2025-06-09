861de6d026ca407db5001e4999433700e207b5a5
2jiyong <jdragon2000@naver.com>
2025-06-09 14:51:39 +0900

    feat: 스프링 시작시 데이터 자동 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO member (id, username, password, email)
+VALUES (1, '1', '1', '1@1');
+INSERT INTO member (id, username, password, email)
+VALUES (2, '2', '2', '2@2');
+INSERT INTO member (id, username, password, email)
+VALUES (3, '3', '3', '3@3');
+INSERT INTO member (id, username, password, email)
+VALUES (4, '4', '4', '4@4');
+INSERT INTO member (id, username, password, email)
+VALUES (5, '5', '5', '5@5');
+INSERT INTO member (id, username, password, email)
+VALUES (6, 'string', 'string', 'string@string');
+
+INSERT INTO article (id, title, content, author_id, created_at)
+VALUES (1, 'title1', 'content1', 1, '2024-05-01T14:23:45');
+INSERT INTO article (id, title, content, author_id, created_at)
+VALUES (2, 'title2', 'content2', 2, '2024-05-01T14:23:45');
+INSERT INTO article (id, title, content, author_id, created_at)
+VALUES (3, 'title6', 'content3', 6, '2024-05-01T14:23:45');
