177edabb2e9a85e4907e0baef2f6c7f4c395ef8b
2jiyong <jdragon2000@naver.com>
2025-06-09 14:01:10 +0900

    feat: JWT 토큰 생성 기능 추가

---
+++ src/main/java/com/example/demo/common/auth/JwtTokenProvider.java
@@ -0,0 +1,46 @@
+package com.example.demo.common.auth;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+
+@Component
+public class JwtTokenProvider {
+  private static final String base64Secret = "bXktdmVyeS1zZWNyZXQta2V5";
+
+  private final long validityInMs = 6 * 3600000;
+  private Key key;
+
+  @PostConstruct
+  public void init() {
+    this.key = Keys.hmacShaKeyFor(java.util.Base64.getUrlDecoder().decode(base64Secret));
+  }
+
+  public String createToken(Long userId, String username) {
+    Claims claims = Jwts.claims().setSubject(username);
+    claims.put("userId", userId);
+
+    Date now = new Date();
+    Date validity = new Date(now.getTime() + validityInMs);
+
+    return Jwts.builder()
+        .setClaims(claims)
+        .setIssuedAt(now)
+        .setExpiration(validity)
+        .signWith(SignatureAlgorithm.HS256, key)
+        .compact();
+  }
+
+  public Claims parseClaims(String token) {
+    return Jwts.parserBuilder().setSigningKey(key).build()
+        .parseClaimsJws(token).getBody();
+  }
+}
