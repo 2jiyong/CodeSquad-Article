d9783dde29c2614d3c820f6f566b2d919a51b468
2jiyong <jdragon2000@naver.com>
2025-06-09 11:45:34 +0900

    feat: Article 엔티티와 repo 추가

---
+++ src/main/java/com/example/demo/article/Article.java
@@ -0,0 +1,33 @@
+package com.example.demo.article;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Article {
+  @Id
+  private Long id;
+
+  @Column(nullable = false, length = 50)
+  private String title;
+  @Column(nullable = false)
+  private String content;
+  @Column(nullable = false)
+  private Long authorId;
+  @CreationTimestamp
+  @Column(nullable = false, length = 20, updatable = false)
+  private LocalDateTime createdAt;
+}
