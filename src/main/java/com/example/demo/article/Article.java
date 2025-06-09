3c9d7b6457754197fb074b2b062c4b4e7809dc40
2jiyong <jdragon2000@naver.com>
2025-06-09 12:29:42 +0900

    refactor: article이 member 객체 갖도록 수정

--- src/main/java/com/example/demo/article/Article.java
+++ src/main/java/com/example/demo/article/Article.java
@@ -4,11 +4,16 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.example.demo.member.Member;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,8 +33,9 @@ public class Article {
   private String title;
   @Column(nullable = false)
   private String content;
-  @Column(nullable = false)
-  private Long authorId;
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "author", nullable = false)
+  private Member author;
   @CreationTimestamp
   @Column(nullable = false, length = 20, updatable = false)
   private LocalDateTime createdAt;
--- src/main/java/com/example/demo/article/ArticleService.java
+++ src/main/java/com/example/demo/article/ArticleService.java
@@ -1,21 +1,35 @@
 package com.example.demo.article;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import com.example.demo.article.dto.ArticleListResponse;
 import com.example.demo.article.dto.ArticleRequest;
+import com.example.demo.article.dto.ArticleResponse;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberRepository;
 
 @Service
 public class ArticleService {
   private final ArticleRepository articleRepository;
+  private final MemberRepository memberRepository;
 
-  public ArticleService(ArticleRepository articleRepository) {
+  public ArticleService(ArticleRepository articleRepository, MemberRepository memberRepository) {
     this.articleRepository = articleRepository;
+    this.memberRepository = memberRepository;
   }
+
+  private static final Member DEFAULT_MEMBER = Member.builder()
+  .id(1L)
+  .username("defaultUser")
+  .password("defaultPassword")
+  .build();
 
   public void addArticle(ArticleRequest request) {
     Article article = Article.builder()
         .title(request.getTitle())
         .content(request.getContent())
-        .authorId(1L) //TODO: 로그인 기능 구현 후 수정
+        .author(DEFAULT_MEMBER) //TODO: 로그인 기능 구현 후 수정
         .build();
 
     articleRepository.save(article);
